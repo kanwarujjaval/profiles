547f03f9470b4716ba6dd1959be5e418d2bd6c0c
Kanwar Ujjaval Singh <ujjavalkanwar@gmail.com>
2013-11-21 22:07:55 +0530

    db.sql 	readme.txt

--- include/config.php
+++ include/config.php
@@ -1,7 +1,7 @@
 <?php
 define("db_server","localhost");
-define("db_user","rudrag_internuse");
-define("db_pass","nmf5mp");
+define("db_user","root");
+define("db_pass","");
 define("db_name","rudrag_intern");
 
 ?>
